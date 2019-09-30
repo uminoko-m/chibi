174b7a3179915a832deb14e67d6ac3bdf7f38405
MASUDA Misa <21716086@jwu.ac.jp>
2019-09-30 16:52:40 +0900

    commit wrong

--- calc.py
+++ calc.py
@@ -1,5 +1,9 @@
 def calc(s):
     nums=list(map(int,s.split("+")))
+    nums2=list(map(int,s.split("*")))
+    print(nums)
+    print(nums2)
+
     count=0
     for i in range(len(nums)):
         count+=nums[i]
@@ -7,4 +11,5 @@ def calc(s):
 
 print(calc("1"))
 print(calc("1+2"))
-print(calc("1+2+3"))
+print(calc("1*2+3"))
+print(calc("1+2*3"))
