2c6924bb2920f7da4740993c4e2fbfc8d6d20c32
MASUDA Misa <21716086@jwu.ac.jp>
2019-10-07 16:14:51 +0900

    added __init__

--- rational.py
+++ rational.py
@@ -1,10 +1,15 @@
 class Q(object):
-    def __init__(self,a,b):
+    def __init__(self,a,b=1):       #初期値を書くと省略可能
         self.a=a
         self.b=b
 
     def __repr__(self):
-        return f'{self.a}/{self.b}'
+        if self.b==1:
+            return str(self.a)
+        else:
+            return f'{self.a}/{self.b}'
 
 q=Q(1,2)
+print(q)
+q=Q(3)
 print(q)
