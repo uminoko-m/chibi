4ab5888f899ee191c3b73d294197874b5e51a361
MASUDA Misa <21716086@jwu.ac.jp>
2019-10-14 16:50:55 +0900

    Binary

--- exp.py
+++ exp.py
@@ -1,6 +1,20 @@
 class Expr:
     def eval(self):pass
 
+def expr(e):
+    if not isinstance(e,Expr):
+        e=Val(e)
+    return e
+
+class Binary(Expr):
+    __slots__=['left','right']
+    def __init__(self,left,right):
+        self.left=expr(left)
+        self.right=expr(right)
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
+
 class Val(Expr):              
     __slots__=['value']
     def __init__(self,value=0):
@@ -10,42 +24,23 @@ class Val(Expr):
     def eval(self):
         return self.value
 
-def expr(e):
-    if not isinstance(e,Expr):
-        e=Val(e)
-    return e
-
-class Add(Expr):
-    __slots__=['left','right']
-    def __init__(self,left,right):
-        self.left=expr(left)
-        self.right=expr(right)
+class Add(Binary):
     def eval(self):
         return self.left.eval() + self.right.eval()
 
-class Mul(Expr):
-    __slots__=['left','right']
-    def __init__(self,left,right):
-        self.left=expr(left)
-        self.right=expr(right)
+class Mul(Binary):
     def eval(self):
         return self.left.eval() * self.right.eval()
 
-class Sub(Expr):
-    __slots__=['left','right']
-    def __init__(self,left,right):
-        self.left=expr(left)
-        self.right=expr(right)
+class Sub(Binary):
     def eval(self):
         return self.left.eval() - self.right.eval()
 
-class Div(Expr):
-    __slots__=['left','right']
-    def __init__(self,left,right):
-        self.left=expr(left)
-        self.right=expr(right)
+class Div(Binary):
     def eval(self):
         return self.left.eval() // self.right.eval()
     
 e=Mul(Add(1,2),3)
+g=Div(2,7)
 assert e.eval()==9
+assert g.eval()==3
