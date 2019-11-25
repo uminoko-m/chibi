ded0bed1410f1de23155ce9026b2950f1fc1c1f8
MASUDA Misa <21716086@jwu.ac.jp>
2019-11-25 16:01:37 +0900

    main

--- icalc.py
+++ icalc.py
@@ -17,6 +17,12 @@ def calc(t):
     print(f'TODO {t.tag})')
     return 0
 
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
 t = parser('1+2*3+4*5')
 print(repr(t))
 print(calc(t))
