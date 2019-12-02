f0b0dc489ec8a50a399272972ddb2f94d182ca27
MASUDA Misa <21716086@jwu.ac.jp>
2019-12-02 15:39:15 +0900

    icalc

--- icalc.py
+++ icalc.py
@@ -6,23 +6,85 @@ Value = { [0-9]+ #Int }
 ''')
 parser = pegpy.generate(peg)
 
+class Expr(object):
+    @classmethod
+    def new(cls,v):
+        if isinstance(v,Expr):
+            return v
+        return Val(v)
 
-def calc(t):
-    if t == 'Int':
-        return int(str(t))
-    if t == 'Add':
-        return calc(t[0]) + calc(t[1])
-    if t == 'Mul':
-        return calc(t[0]) * calc(t[1])
-    print(f'TODO {t.tag})')
-    return 0
+class Val(Expr):              
+    __slots__=['value']
+
+    def __init__(self,value):
+        self.value = value
+
+    def __repr__(self):
+        return f'Val({self.value})'
+
+    def eval(self,env:dict):
+        return self.value
+
+e=Val(0)
+assert e.eval({})==0
+
+class Binary(Expr):
+    __slots__=['left','right']
+
+    def __init__(self,left,right):
+        self.left=Expr.new(left)
+        self.right=Expr.new(right)
+
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
+
+class Add(Binary):
+    def eval(self,env:dict):
+        return self.left.eval(env) + self.right.eval(env)
+
+class Mul(Binary):
+    def eval(self,env:dict):
+        return self.left.eval(env) * self.right.eval(env)
+
+class Sub(Binary):
+    def eval(self,env:dict):
+        return self.left.eval(env) - self.right.eval(env)
+
+class Div(Binary):
+    def eval(self,env:dict):
+        return self.left.eval(env) // self.right.eval(env)
+
+def conv(tree):
+    if tree=='Block':
+        return conv(tree[0])
+    if tree == 'Val' or tree=='Int':
+        return Val(int(str(tree)))
+    if tree == 'Add':
+        return Add(conv(tree[0]),conv(tree[1]))
+    print('`TODO',tree.tag)
+    return Val(str(tree))
+
+def run(stc:str):
+    tree=parser(stc)
+    if tree.isError():
+        print(repr(tree))
+    else:
+        e=conv(tree)
+        print(repr(e))
+        print(e.eval({}))
 
 def main():
-    pass
+    try:
+        while True:
+            s = input('>>> ')
+            if s=='':
+                break
+            run(s)
+
+    except EOFError:
+        return
 
 if __name__ == '__main__':
     main()
 
-t = parser('1+2*3+4*5')
-print(repr(t))
-print(calc(t))
